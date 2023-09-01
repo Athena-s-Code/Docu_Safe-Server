cac7b9df9df3a731d20a67a6290345886500e7d7
Pasindu Premachandra <pasindug98@gmail.com>
2023-09-01 16:17:09 +0530

    classification api added

--- server.py
+++ server.py
@@ -1,38 +1,47 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, jsonify
 from flask_cors import CORS
 import os
-import pickle
+
+import util_classifier
 
 app = Flask(__name__)
 CORS(app)
 
 CORS(app, resources={r"/upload": {"origins": "http://localhost:3000"}})
 
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+UPLOAD_FOLDER = "static/files/"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
 
-@app.route('/upload', methods=['POST'])
+@app.route("/upload", methods=["POST"])
 def upload_file():
-    if 'file' not in request.files:
+    if "file" not in request.files:
         return "No file part", 400
 
-    file = request.files['file']
+    file = request.files["file"]
 
-    if file.filename == '':
+    if file.filename == "":
         return "No selected file", 400
 
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+    file.save(os.path.join(app.config["UPLOAD_FOLDER"], file.filename))
     return "File uploaded successfully", 200
 
 
-@app.route('/get_pdf/<filename>')
+@app.route("/get_pdf/<filename>")
 def get_pdf(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
 
-if __name__ == '__main__':
+@app.route("/get_classifications")
+def get_classifications():
+    response = jsonify({"classifications": util_classifier.get_confidentiality()})
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
+
+if __name__ == "__main__":
+    util_classifier.load_saved_artifacts()
     app.run(debug=True)
